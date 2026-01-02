57619ce2964253d43e7109624aece2317c12a0ae
DGbolaga <gbolagadaramola765@gmail.com>
2026-01-02 23:20:08 +0100

    Time: 3 ms (23.49%), Space: 17.1 MB (97.46%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def f(i, j,dp):
+            #base case
+            if i == 0 and j == 0: return 1
+            if i < 0 or j < 0: return 0
+            if dp[i][j] != -1: return dp[i][j]
+
+            #explore paths.
+            up = f(i-1, j, dp)
+            left = f(i, j-1, dp)
+            
+            dp[i][j] = up + left
+            return dp[i][j]
+        
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        return f(m-1, n-1, dp)
